67f6656f95eba851acd9c98fe543a4f77d760423
Dario Cubero <dariocuberozgz@gmail.com>
2022-10-02 09:17:11 +0200

    progress bar corrected

--- js/control.js
+++ js/control.js
@@ -1,5 +1,25 @@
-
 $( document ).ready(function() {
-    console.log( "ready!" );
-    
+    var diapositiva = 0;
+
+    $(document).keydown(function(e) {
+        if(e.originalEvent.code == "ArrowRight" || e.originalEvent.code == 'Space'){
+            console.log(diapositiva);
+            if(diapositiva<8){
+                diapositiva = diapositiva+1;
+            }else if(diapositiva==8){
+                $('#progressbar').css("width", "0");
+                return;
+            }
+            var porcentaje_actual = diapositiva*12.5;
+            $('#progressbar').css("width", porcentaje_actual+'%');
+            console.log(porcentaje_actual);
+        } else if(e.originalEvent.code == "ArrowLeft"){
+            if(diapositiva>0){
+                diapositiva = diapositiva-1;
+            }
+            var porcentaje_actual = diapositiva*12.5
+            $('#progressbar').css("width", porcentaje_actual+'%');
+        }
+    });
+
 });
